c16fc2e00305fc9da455d0ccbe1d088dd972e6f3
h2jaeee <84111351+h2jaeee@users.noreply.github.com>
2021-05-13 11:46:10 +0900

    강사님이 만드신 코드(baseline)
    레퍼지토리에 파일올리기 테스트

---
+++ may11_teacher.java
@@ -0,0 +1,27 @@
+
+public class may11_teacher {
+	   public static void main(String[] args) 
+	   {
+		   int val=13;
+		   int div=2;
+		   boolean isPrimeNum = true;
+		   for(int i=0;i<val-2;i++)
+		   {
+			   if(0==val%div)
+			   {
+				   isPrimeNum = false;
+				   break;
+			   }
+			   div++;
+		   }
+		   if(isPrimeNum==true)
+		   {
+			   System.out.println("소수");		  
+		   }
+		   else
+		   {
+			   System.out.println("합성수");
+		   }
+	   }
+
+}
